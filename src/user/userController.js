09b7176de0ff36a15a535bb36747e47028a6f60c
Crescens-Henry <crescencio.ps.code@gmail.com>
2023-02-16 21:50:57 -0600

    first commit

--- src/user/userController.js
+++ src/user/userController.js
@@ -1,36 +1,96 @@
-var userService = require('./userServices');
+var userService = require("./userServices");
 
-var createUserControllerFunc = async (req, res) =>  {
-    try {
-    console.log(req.body);
+var createUserControllerFunc = async (req, res) => {
+  try {
     var status = await userService.createUserDBService(req.body);
     console.log(status);
-
     if (status) {
-        res.send({ "status": true, "message": "Usuario creado" });
+      res.send({ status: true, message: "Usuario creado" });
     } else {
-        res.send({ "status": false, "message": "Error creando usuario" });
+      res.send({ status: false, message: "Error creando usuario" });
     }
-    }
-    catch(err) {
-        console.log(err);
-    }
-}
+  } catch (err) {
+    console.log(err);
+  }
+};
 
 var loginUserControllerFunc = async (req, res) => {
-    var result = null;
-    try {
-        result = await userService.loginuserDBService(req.body);
-        if (result.status) {
-            res.send({ "status": true, "message": result.msg });
-        } else {
-            res.send({ "status": false, "message": result.msg });
-        }
-
-    } catch (error) {
-        console.log(error);
-        res.send({ "status": false, "message": error.msg });
+  try {
+    var result = await userService.loginuserDBService(req.body);
+    console.log(`${result.status}`);
+    if (result.status) {
+      res.send({ status: true, message: result.msg });
+    } else {
+      res.send({ status: false, message: result.msg });
     }
-}
+  } catch (error) {
+    console.log(error);
+    res.send({ status: false, message: error.msg });
+  }
+};
+const userCollectionFunc = async (req, res) => {
+  try {
+    var response = await userService.userCollection(req.body); //aqui manda el objeto del postman
+    console.log(response.status);
+    if (response.status) {
+      res.send({ status: true, message: response.msg });
+    } else {
+      res.send({ status: false, message: response.msg });
+    }
+  } catch (error) {
+    console.log(error);
+    res.send({ status: false, message: error.msg });
+  }
+};
 
-module.exports = { createUserControllerFunc, loginUserControllerFunc };
+const searchUserFunc = async (req, res) => {
+  try {
+    var response = await userService.searchUser(req.body); //aqui manda el objeto del postman
+    console.log(response.status);
+    if (response.status) {
+      res.send({ status: true, message: response.msg });
+    } else {
+      res.send({ status: false, message: response.msg });
+    }
+  } catch (error) {
+    console.log(error);
+    res.send({ status: false, message: error.msg });
+  }
+};
+const deleteUserFunc = async (req, res) => {
+  try {
+    var response = await userService.deleteUser(req.body); //aqui manda el objeto del postman
+    console.log(response.status);
+    if (response.status) {
+      res.send({ status: true, message: response.msg });
+    } else {
+      res.send({ status: false, message: response.msg });
+    }
+  } catch (error) {
+    console.log(error);
+    res.send({ status: false, message: error.msg });
+  }
+};
+const updateUserFunc = async (req, res) => {
+  try {
+    var response = await userService.updateUser(req.body); //aqui manda el objeto del postman
+    console.log(response.status);
+    if (response.status) {
+      res.send({ status: true, message: response.msg });
+    } else {
+      res.send({ status: false, message: response.msg });
+    }
+  } catch (error) {
+    console.log(error);
+    res.send({ status: false, message: error.msg });
+  }
+};
+
+module.exports = {
+  createUserControllerFunc,
+  loginUserControllerFunc,
+  searchUserFunc,
+  userCollectionFunc,
+  deleteUserFunc,
+  updateUserFunc,
+};
