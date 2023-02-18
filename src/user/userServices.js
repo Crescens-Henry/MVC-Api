dcb4fdb5321d8478a504312b07a31c42bfbf4d70
Crescens-Henry <crescencio.ps.code@gmail.com>
2023-02-17 22:12:03 -0600

    Final commit practice

--- src/user/userServices.js
+++ src/user/userServices.js
@@ -151,11 +151,6 @@ module.exports.updateUser = (userDetails) => {
         } else {
           if (result != undefined && result != null) {
             console.log(result);
-            var decrypted = encryptor.encrypt(result.password);
-            userModel.find(
-              { email: userDetails.email },
-              { password: decrypted }
-            );
             if (result.email == userDetails.email) {
               resolve({
                 status: true,
